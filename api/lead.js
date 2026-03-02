f3c25df2319398234360bc508d320d3e5c2ce078
treza8347-a11y <treza8347@gmail.com>
2026-03-03 00:45:23 +0100

    Add API handler for POST requests to Zapier

---
+++ api/lead.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  try {
+    const response = await fetch("https://hooks.zapier.com/hooks/catch/26582458/u0wuaju/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    return res.status(200).json({ message: "Success" });
+
+  } catch (error) {
+    return res.status(500).json({ message: "Error" });
+  }
+}
