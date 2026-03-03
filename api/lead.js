0231d1509714a65a2be7e36602c7ea99d8edbf0f
treza8347-a11y <treza8347@gmail.com>
2026-03-03 03:03:31 +0100

    Add POST handler for Zapier webhook integration

--- api/lead.js
+++ api/lead.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
 
+if (req.method !== "POST") {
+return res.status(405).send("Method not allowed");
+}
+
+try {
+
+await fetch("https://hooks.zapier.com/hooks/catch/26582458/u0wuaju/", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify(req.body)
+});
+
+return res.status(200).send("OK");
+
+} catch (error) {
+return res.status(500).send("Server Error");
+}
+
+}
